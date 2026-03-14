036c884056e4a077a5712b70387cead2f7f99b66
Nishant Mishra <103406408+nishant35mishra@users.noreply.github.com>
2026-03-14 14:05:48 +0530

    Refactor star pattern printing into a separate method

--- Solution1.java
+++ Solution1.java
@@ -1,44 +1,22 @@
 public class StarPattern {
-    public static void main(String[] args) {
-        int n = 5; 
-
-        
-        for (int i = 1; i <= n; i++) {
-            
-            for (int j = i; j < n; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                if (j == 1 || j == i) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-                if (j < i) {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+    private static void printRow(int spaces, int stars) {
+        for (int j = 0; j < spaces; j++) System.out.print(" ");
+        for (int j = 1; j <= stars; j++) {
+            System.out.print((j == 1 || j == stars) ? "*" : " ");
+            if (j < stars) System.out.print(" ");
         }
+        System.out.println();
+    }
 
-        
-        for (int i = n - 1; i >= 1; i--) {
-            
-            for (int j = n; j > i; j--) {
-                System.out.print(" ");
-            }
-            
-            for (int j = 1; j <= i; j++) {
-                if (j == 1 || j == i) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-                if (j < i) {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+    public static void main(String[] args) {
+        final int ROWS = 5;
+
+        for (int i = 1; i <= ROWS; i++) {
+            printRow(ROWS - i, i);
+        }
+        for (int i = ROWS - 1; i >= 1; i--) {
+            printRow(ROWS - i, i);
         }
     }
 }
+
